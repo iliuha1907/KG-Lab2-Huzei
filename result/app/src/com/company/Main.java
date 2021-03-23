70f08152ff97d0ca2119cfcb6e169d138760040d
Ilya Huzei <iaguzey5@gmail.com>
2021-03-23 13:03:19 +0300

    Add files

---
+++ result/app/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashSet<? extends Number>numbers = new HashSet<>();
+        numbers.add(1);
+    }
+}
